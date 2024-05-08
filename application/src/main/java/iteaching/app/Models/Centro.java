d4e2d22c6c9909bdcd1dbb33192152a06ce22eb8
Francisco <fj.anillo@soprasteria.com>
2024-05-08 17:33:44 +0200

    Anotaciones Clase Centro

--- application/src/main/java/iteaching/app/Models/Centro.java
+++ application/src/main/java/iteaching/app/Models/Centro.java
@@ -1,6 +1,17 @@
 package iteaching.app.Models;
 
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Center")
 public class Centro {
 
     private String nombre;
